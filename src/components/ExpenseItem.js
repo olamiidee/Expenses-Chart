c5991e4f31f59351636d479638f7b82619556c38
olamiidee <osibeluwoolamide@gmail.com>
2022-10-17 00:55:41 +0100

    udemy don wreck my data sha

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,21 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US",{month: "short"})
+  const year = props.date.toLocaleString("en-US",{year: "numeric"})
+  const day = props.date.toLocaleString("en-US",{year: "numeric"})
 
   return (
     <div>
-      <div>{props.date}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div>
         <h2>{props.title}</h2>
-        <div>{props.amount}</div>
+        <div>${props.amount}</div>
       </div>
-
     </div>
   );
 }
