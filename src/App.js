2adb7775fd6aecfbb5ab6a180dc9c6c2cf33f9fe
Olamiidee <osibeluwoolamide@gmail.com>
2022-11-18 23:42:18 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import ExpenseItem from "./components/ExpenseItem";
-import "./index.css"
+import "./index.css";
 
 const App = () => {
   const items = [
@@ -26,30 +29,31 @@ const App = () => {
 
   return (
     <div>
-    <div className="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
-    </div></div>
-    
+      <NewExpense />
+      <div className="expenses">
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,28 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
-import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  
+export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <div className="flex justify-between items-center p-[0.5rem] bg-[#4b4b4b] rounded-xl expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="flex flex-row gap-4 items-center justify-start expense-item__description">
-        <h2 className="text-[1.25rem] text-white">{props.title}</h2>
-        <div className="bg-[#40005d] text-[1.25rem] font-bold text-white border border-white rounded-[12px] py-[0.5rem] px-[1.5rem]">${props.amount}</div>
+        <h2 className="text-[1.25rem] text-white">{title}</h2>
+        <div className="bg-[#40005d] text-[1.25rem] font-bold text-white border border-white rounded-[12px] py-[0.5rem] px-[1.5rem]">
+          ${props.amount}
+        </div>
       </div>
+      <button
+        className="border rounded-xl p-2 ml-2 text-white"
+        onClick={clickHandler}
+      >
+        Change Title
+      </button>
     </div>
   );
 }
-
-export default ExpenseItem;
